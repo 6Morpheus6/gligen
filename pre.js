07ac5c6eaaba697e8ea85d55fe5cf1fc03a29dc2
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2024-11-02 04:14:21 +0100

    synced with comfy

--- pre.js
+++ pre.js
@@ -2,13 +2,13 @@ module.exports = (config, kernel) => {
   const x = {
     "win32": {
       "nvidia": `pip install torch torchvision torchaudio ${config.xformers ? 'xformers' : ''} --index-url https://download.pytorch.org/whl/cu121`,
-      "amd": "pip install torch-directml",
+      "amd": "pip install torch-directml torchaudio torchvision numpy==1.26.4",
       "cpu": "pip install torch torchvision torchaudio"
     },
-    "darwin": "pip install torch torchvision torchaudio",
+    "darwin": "pip install --pre torch==2.3.1 torchvision torchaudio==2.3.1",
     "linux": {
-      "nvidia": `pip install torch torchvision torchaudio ${config.xformers ? 'xformers' : ''}`,
-      "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7",
+      "nvidia": `pip install torch torchvision torchaudio ${config.xformers ? 'xformers' : ''} --index-url https://download.pytorch.org/whl/cu121`,
+      "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.0",
       "cpu": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu"
     }
   }
